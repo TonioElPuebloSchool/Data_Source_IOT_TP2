300a411d61b270434dcd53694e0cc5c95979f286
Antoine COURBI <48368458+tonioelpueblo@users.noreply.github.com>
2023-12-13 12:40:34 +0100

    step 9

--- services/epf-flower-data-science/src/api/routes/data.py
+++ services/epf-flower-data-science/src/api/routes/data.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from src.schemas.message import MessageResponse
-from src.services.data import download_dataset, load_iris_dataset, process_data
-
+from src.services.data import download_dataset, load_iris_dataset, process_data, split_train_test
 
 router = APIRouter()
 
@@ -17,3 +16,6 @@ def loading():
 def process():
     return process_data()
 
+@router.get("/split_train_test")
+def split(test_size: float = 0.2, random_state: int = 42):
+    return split_train_test(test_size, random_state)
--- services/epf-flower-data-science/src/services/data.py
+++ services/epf-flower-data-science/src/services/data.py
@@ -2,6 +2,8 @@ import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
 from src.schemas.message import MessageResponse
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import json
 
 def download_dataset():
     api = KaggleApi()
@@ -18,3 +20,8 @@ def process_data():
     numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
     df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
     return df.to_dict(orient='records')
+
+def split_train_test(test_size=0.2, random_state=42):
+    df = pd.read_csv('data/Iris.csv')
+    train_df, test_df = train_test_split(df, test_size=test_size, random_state=random_state)
+    return train_df.to_dict(orient='records'), test_df.to_dict(orient='records')
